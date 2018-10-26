65a6612f43566393f5ce58de5f94e7b3e6d6876e
Sergey Pozhilov <pozhilov@gmail.com>
2018-10-26 17:15:49 +0300

    bootstrap in vendor.js, customizer code corrected; headroom in main js

--- assets/js/src/main.js
+++ assets/js/src/main.js
@@ -1,3 +1,30 @@
 'use strict';
-
+const $ = require( 'jquery' );
+import Headroom from 'node_modules/headroom.js/dist/headroom.js';
 require ( './customizer' );
+
+
+const headroomOptions = {
+  'tolerance': 20,
+  'offset': 80,
+  'classes': {
+    'initial': 'animated',
+    'pinned': 'slideDown',
+    'unpinned': 'slideUp'
+  }
+};
+
+const header = document.querySelector( '.headroom' );
+let headroom = new Headroom( header );
+headroom.init( headroomOptions );
+
+
+$( window ).scroll( function() {
+  if ( 140 <  $( this ).scrollTop() ) {
+    $( '.headroom' ).removeClass( 'ontop-now' );
+    $( '.navbar-dual' ).removeClass( 'navbar-inverse' );
+  } else {
+    $( '.headroom' ).addClass( 'ontop-now' );
+    $( '.navbar-dual' ).addClass( 'navbar-inverse' );
+  }
+});

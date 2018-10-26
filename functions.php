05e8f31d79d5c36ff528206efb7ffb33309b3dbb
Sergey Pozhilov <pozhilov@gmail.com>
2018-10-26 12:03:16 +0300

    Functions: navigation walker, js/css loading

--- functions.php
+++ functions.php
@@ -7,6 +7,13 @@
  * @package Progressus
  */
 
+/*
+ * Register Custom Navigation Walker
+ * @link https://github.com/wp-bootstrap/wp-bootstrap-navwalker
+ */
+require_once get_template_directory() . '/inc/class-wp-bootstrap-navwalker.php';
+
+
 if ( ! function_exists( 'progressus_setup' ) ) :
 	/**
 	 * Sets up theme defaults and registers support for various WordPress features.
@@ -37,14 +44,13 @@ if ( ! function_exists( 'progressus_setup' ) ) :
 
 		/*
 		 * Enable support for Post Thumbnails on posts and pages.
-		 *
 		 * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
 		 */
 		add_theme_support( 'post-thumbnails' );
 
 		// This theme uses wp_nav_menu() in one location.
 		register_nav_menus( array(
-			'menu-1' => esc_html__( 'Primary', 'progressus' ),
+			'primary' => esc_html__( 'Primary', 'progressus' ),
 		) );
 
 		/*
@@ -61,7 +67,7 @@ if ( ! function_exists( 'progressus_setup' ) ) :
 
 		// Set up the WordPress core custom background feature.
 		add_theme_support( 'custom-background', apply_filters( 'progressus_custom_background_args', array(
-			'default-color' => 'ffffff',
+			'default-color' => '777777',
 			'default-image' => '',
 		) ) );
 
@@ -120,11 +126,15 @@ add_action( 'widgets_init', 'progressus_widgets_init' );
  * Enqueue scripts and styles.
  */
 function progressus_scripts() {
-	wp_enqueue_style( 'progressus-style', get_stylesheet_uri() );
+    // Theme-specific stylesheet
+    wp_enqueue_style( 'progressus-base-style', get_template_directory_uri() . "/assets/css/styles.css", false, '1.0', 'all' );
 
-	wp_enqueue_script( 'progressus-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
+    // Default Style file - style.css
+    wp_enqueue_style( 'progressus-style', get_stylesheet_uri() );
 
-	wp_enqueue_script( 'progressus-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
+    // Scripts
+    wp_enqueue_script( 'progressus-vendor', get_template_directory_uri() . "/assets/js/vendor.js", ['jquery'], '1.0', true );
+    wp_enqueue_script( 'progressus-main', get_template_directory_uri() . "/assets/js/main.js", ['jquery', 'customize-preview'], '1.0', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );

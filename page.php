a5d139c431b4b9effc92b371be3e6da966338677
Sergey Pozhilov <pozhilov@gmail.com>
2018-10-27 20:15:06 +0300

    Sidebar is on its place in page template

--- page.php
+++ page.php
@@ -13,34 +13,40 @@
  */
 
 get_header();
+
+$content_class = is_active_sidebar('sidebar-1') ? 'main-content' : '';
 ?>
 
 <div id="primary" class="content-area container mt-5">
-    <?php
-    while (have_posts()) : the_post(); ?>
+    <div class="row">
+        <div class="<?php echo $content_class; ?>">
+        <?php
+        while (have_posts()) : the_post(); ?>
 
-        <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-            <?php progressus_post_thumbnail(); ?>
+            <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+                <?php progressus_post_thumbnail(); ?>
 
-            <div class="entry-content content">
-                <?php
-                the_content();
-                wp_link_pages( array(
-                    'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'progressus' ),
-                    'after'  => '</div>',
-                ) );
-                ?>
-            </div>
-        </article>
+                <div class="entry-content content">
+                    <?php
+                    the_content();
+                    wp_link_pages( array(
+                        'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'progressus' ),
+                        'after'  => '</div>',
+                    ) );
+                    ?>
+                </div>
+            </article>
 
-    <?php
-        // If comments are open or we have at least one comment, load up the comment template.
-        if (comments_open() || get_comments_number()) :
-            comments_template();
-        endif;
-    endwhile;
-    ?>
-    <?php get_sidebar(); ?>
+        <?php
+            // If comments are open or we have at least one comment, load up the comment template.
+            if (comments_open() || get_comments_number()) :
+                comments_template();
+            endif;
+        endwhile;
+        ?>
+        </div>
+        <?php get_sidebar(); ?>
+    </div>
 </div>
 
 <?php

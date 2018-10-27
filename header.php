c10b8b9182310e4311e45e69d403baeda07bbec1
Sergey Pozhilov <pozhilov@gmail.com>
2018-10-27 18:03:07 +0300

    move page header code to header.php

--- header.php
+++ header.php
@@ -51,3 +51,26 @@
             ?>
         </div>
     </div>
+
+    <?php
+    // TODO: import appropriate header below, which one - depends on the type of the page.
+    ?>
+    <header class="entry-header head-inner"
+            style="background-image: url(<?php echo get_post_meta( $post->ID, 'post_imageheader', true);?>); background-size: cover;">
+        <div class="container">
+            <div class="row">
+                <?php
+                $post_id = get_the_ID();
+                $post_title = get_post_meta($post_id, 'post_title', true);
+                if (empty($post_title)) $post_title = the_title();
+                $post_caption = get_post_meta($post_id, 'post_caption', true);
+                ?>
+                <h1 class="page-title text-center">
+                    <?php
+                    echo $post_title;
+                    if (!empty($post_caption)) echo "<small>$post_caption</small>";
+                    ?>
+                </h1>
+            </div>
+        </div>
+    </header>
--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -10,30 +10,6 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-    <header class="entry-header head-inner"
-            style="background-image: url(<?php echo get_post_meta( $post->ID, 'post_imageheader', true);?>); background-size: cover;">
-        <div class="container">
-            <div class="row">
-                <?php
-                $post_id = get_the_ID();
-                $post_title = get_post_meta($post_id, 'post_title', true);
-                if (empty($post_title)) $post_title = the_title();
-                $post_caption = get_post_meta($post_id, 'post_caption', true);
-                ?>
-                <h1 class="page-title text-center">
-                    <?php
-                    echo $post_title;
-                    if (!empty($post_caption)) echo "<small>$post_caption</small>";
-                    ?>
-                </h1>
-            </div>
-        </div>
-    </header>
-
-    <?php
-    // TODO: import appropriate header, which one - depends on the type of the page.
-    ?>
-
     <?php progressus_post_thumbnail(); ?>
 
 	<div class="entry-content content">

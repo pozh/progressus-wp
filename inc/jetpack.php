f01d74ae2f5608ff9025f675a01c0e4059f2bdc3
Sergey Pozhilov <pozhilov@gmail.com>
2018-10-27 14:21:56 +0300

    Got rid of infinite scroll feature (as useless in this particular theme)

--- inc/jetpack.php
+++ inc/jetpack.php
@@ -15,12 +15,6 @@
  * See: https://jetpack.com/support/content-options/
  */
 function progressus_jetpack_setup() {
-	// Add theme support for Infinite Scroll.
-	add_theme_support( 'infinite-scroll', array(
-		'container' => 'main',
-		'render'    => 'progressus_infinite_scroll_render',
-		'footer'    => 'page',
-	) );
 
 	// Add theme support for Responsive Videos.
 	add_theme_support( 'jetpack-responsive-videos' );
@@ -43,17 +37,3 @@ function progressus_jetpack_setup() {
 	) );
 }
 add_action( 'after_setup_theme', 'progressus_jetpack_setup' );
-
-/**
- * Custom render function for Infinite Scroll.
- */
-function progressus_infinite_scroll_render() {
-	while ( have_posts() ) {
-		the_post();
-		if ( is_search() ) :
-			get_template_part( 'template-parts/content', 'search' );
-		else :
-			get_template_part( 'template-parts/content', get_post_type() );
-		endif;
-	}
-}

f4b900a6f1617382c69e5d7adf4ab69c3e15420a
Sergey Pozhilov <pozhilov@gmail.com>
2018-10-27 19:35:29 +0300

    Refactoring page.php: got rid of include

--- page.php
+++ page.php
@@ -17,18 +17,31 @@ get_header();
 
 <div id="primary" class="content-area container mt-5">
     <?php
-    while (have_posts()) :
-        the_post();
-        get_template_part('template-parts/content', 'page');
+    while (have_posts()) : the_post(); ?>
 
+        <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+            <?php progressus_post_thumbnail(); ?>
+
+            <div class="entry-content content">
+                <?php
+                the_content();
+                wp_link_pages( array(
+                    'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'progressus' ),
+                    'after'  => '</div>',
+                ) );
+                ?>
+            </div>
+        </article>
+
+    <?php
         // If comments are open or we have at least one comment, load up the comment template.
         if (comments_open() || get_comments_number()) :
             comments_template();
         endif;
     endwhile;
     ?>
+    <?php get_sidebar(); ?>
 </div>
 
 <?php
-get_sidebar();
 get_footer();

ebb45593abd169f78993e30e9c286146cff4eb90
Sergey Pozhilov <pozhilov@gmail.com>
2018-10-28 13:27:55 +0300

    Page title - missing post ID - corrected

--- template-parts/header-page.php
+++ template-parts/header-page.php
@@ -4,7 +4,7 @@
         <?php
         $post_id = get_the_ID();
         $post_title = get_post_meta($post_id, 'post_title', true);
-        if (empty($post_title)) $post_title = get_the_title();
+        if (empty($post_title)) $post_title = get_the_title($post_id);
         $post_caption = get_post_meta($post_id, 'post_caption', true);
         ?>
         <h1 class="page-title text-center">
